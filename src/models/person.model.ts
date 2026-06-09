3dd596db0ba809f7c2b875c302c674373fa642c2
ukeshmahara <ukeshmahara361@gmail.com>
2026-06-09 20:33:12 +0545

    firstcommit sprint3

---
+++ src/models/person.model.ts
@@ -0,0 +1,7 @@
+import { Person } from "../types/person.type";
+
+export const dataset: Person[] = [
+    { id: 1, name: "Ram", age: 30 },
+    { id: 2, name: "Hari", age: 25 },
+    { id: 3, name: "Bob", age: 35 }
+];
